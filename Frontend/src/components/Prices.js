6125bc06b739a4916f45543698fcc148c0d8d7b8
Ashish-1581 <125173820+Ashish-1581@users.noreply.github.com>
2023-11-22 02:00:01 +0530

    update prices

--- Frontend/src/components/Prices.js
+++ Frontend/src/components/Prices.js
@@ -1,32 +1,32 @@
 export const Prices = [
   {
     _id: 0,
-    name: "$0 to 19",
-    array: [0, 19],
+    name: "$0 to 99",
+    array: [0, 99],
   },
   {
     _id: 1,
-    name: "$20 to 39",
-    array: [20, 39],
+    name: "$100 to 199",
+    array: [100, 199],
   },
   {
     _id: 2,
-    name: "$40 to 59",
-    array: [40, 59],
+    name: "$200 to 299",
+    array: [200, 299],
   },
   {
     _id: 3,
-    name: "$60 to 79",
-    array: [60, 79],
+    name: "$300 to 399",
+    array: [300, 399],
   },
   {
     _id: 4,
-    name: "$80 to 99",
-    array: [80, 99],
+    name: "$400 to 499",
+    array: [400, 499],
   },
   {
     _id: 4,
-    name: "$100 or more",
-    array: [100, 9999],
+    name: "$500 or more",
+    array: [500, 9999],
   },
 ];
